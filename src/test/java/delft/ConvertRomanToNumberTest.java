2e18adb84744c6bd98cff53eac01a6c4fac4a14f
Landon Rohr <landonroar@gmail.com>
2024-10-23 17:20:28 -0500

    Created a few tests

--- src/test/java/delft/ConvertRomanToNumberTest.java
+++ src/test/java/delft/ConvertRomanToNumberTest.java
@@ -1,16 +1,46 @@
 package delft;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ConvertRomanToNumberTest {
 
-    @Test
-    public void ourFirstTest(){
-
+    @ParameterizedTest(name = "converter")
+    @MethodSource("generator")
+    void convert(String description, String input, int expected) {
+       int result = ConvertRomanToNumber.convertStringToNumber(input);
+        assertEquals(expected, result);
     }
 
+    public static Stream<Arguments> generator() {
+    Arguments tc1 = Arguments.of("one", "I", 1);
+    Arguments tc2 = Arguments.of("white space string", " ", 0);//edge case
+    Arguments tc3 = Arguments.of("four", "IV", 4); //smaller numeral in front
+    Arguments tc4 = Arguments.of("six", "VI", 6); //bigger numeral in front
+    Arguments tc5 = Arguments.of("a lot of ones", "IIIIIIIIIII", 11);
 
+    return Stream.of(tc1,tc2,tc3,tc4,tc5,tc5);
+    }
+
+    @ParameterizedTest(name = "add")
+    @MethodSource("generatorAdd")
+    void add(String description, String romanNum1, String romanNum2, int expected) {
+        int result = ConvertRomanToNumber.add(romanNum1, romanNum2);
+        assertEquals(expected, result);
+    }
+
+    public static Stream<Arguments> generatorAdd() {
+        Arguments tc1 = Arguments.of("nothing", " "," ", 0);
+        Arguments tc2 = Arguments.of("only one val", "V"," ", 5);
+        Arguments tc3 = Arguments.of("adding two", "V","II", 7);
+
+        return Stream.of(tc1, tc2, tc3);
+    }
 }
